d6f603a0dacd69fd1546957ee912b76ac252b677
winterrr03 <dong.huynhquang21503@gmail.com>
2024-06-24 20:01:36 +0700

    feat(song): detail song

--- controllers/client/song.controller.ts
+++ controllers/client/song.controller.ts
@@ -30,8 +30,36 @@ export const list = async (req: Request, res: Response) => {
   }
   
   res.render("client/pages/songs/list", {
-    pageTitle: "Danh sách bài hát",
+    pageTitle: topic.title,
     topic: topic,
     songs: songs
+  });
+};
+
+// [GET] /songs/detail/:slugSong
+export const detail = async (req: Request, res: Response) => {
+  const slugSong: string = req.params.slugSong;
+
+  const song = await Song.findOne({
+    slug: slugSong,
+    deleted: false,
+    status: "active"
+  });
+
+  const singer = await Singer.findOne({
+    _id: song.singerId,
+    deleted: false
+  }).select("fullName");
+
+  const topic = await Topic.findOne({
+    _id: song.topicId,
+    deleted: false
+  }).select("title");
+
+  res.render("client/pages/songs/detail", {
+    pageTitle: song.title,
+    song: song,
+    singer: singer,
+    topic: topic
   });
 };
--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,31 @@
-console.log("OK");
+// APlayer
+const aplayer = document.querySelector("#aplayer");
+if (aplayer) {
+  let dataSong = aplayer.getAttribute("data-song");
+  dataSong = JSON.parse(dataSong);
+
+  let dataSinger = aplayer.getAttribute("data-singer");
+  dataSinger = JSON.parse(dataSinger);
+
+  const ap = new APlayer({
+    container: aplayer,
+    audio: [{
+      name: dataSong.title,
+      artist: dataSinger.fullName,
+      url: dataSong.audio,
+      cover: dataSong.avatar
+    }],
+    autoplay: true
+  });
+
+  const avatar = document.querySelector(".singer-detail .inner-avatar");
+
+  ap.on('play', function () {
+    avatar.style.animationPlayState = "running";
+  });
+
+  ap.on('pause', function () {
+    avatar.style.animationPlayState = "paused";
+  });
+}
+// End APlayer
