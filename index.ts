cfd054774c8aa9f62537d6c06e088244a0f19a72
winterrr03 <dong.huynhquang21503@gmail.com>
2024-06-24 15:39:54 +0700

    create project

---
+++ index.ts
@@ -0,0 +1,21 @@
+import express, { Express, Request, Response } from "express";
+import env from "dotenv";
+env.config();
+
+import { connect } from "./config/database";
+
+connect();
+
+const app: Express = express();
+const port: (number | string) = `${process.env.PORT}` || 3000;
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+app.get("/topics", (req: Request, res: Response) => {
+  res.render("client/pages/topics/index")
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
