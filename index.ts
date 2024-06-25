ff8d59639915ea203899021db9761af5fcde05a1
winterrr03 <dong.huynhquang21503@gmail.com>
2024-06-25 10:20:34 +0700

    feat(admin): layout admin

--- index.ts
+++ index.ts
@@ -3,6 +3,10 @@ import env from "dotenv";
 env.config();
 
 import { connect } from "./config/database";
+import adminRoutes from "./routes/admin/index.route";
+import clientRoutes from "./routes/client/index.route";
+import { systemConfig } from "./config/system";
+import path from "path";
 
 connect();
 
@@ -14,8 +18,17 @@ app.set("view engine", "pug");
 
 app.use(express.static("public"));
 
-import clientRoutes from "./routes/client/index.route";
+// TinyMCE
+app.use(
+  "/tinymce",
+  express.static(path.join(__dirname, "node_modules", "tinymce"))
+);
+// End TinyMCE
 
+// App Local Variables
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
+
+adminRoutes(app);
 clientRoutes(app);
 
 app.listen(port, () => {
