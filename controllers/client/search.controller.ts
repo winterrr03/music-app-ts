2982d30e046952e5e08613f0d0ffdc8e7570b432
winterrr03 <dong.huynhquang21503@gmail.com>
2024-06-25 09:28:36 +0700

    feat(search): advance, suggest search

---
+++ controllers/client/search.controller.ts
@@ -0,0 +1,61 @@
+import { Request, Response } from "express";
+import Song from "../../models/song.model";
+import Singer from "../../models/singer.model";
+import unidecode from "unidecode";
+
+// [GET] /search/:type
+export const result = async (req: Request, res: Response) => {
+  const type = req.params.type;
+
+  const keyword: string = `${req.query.keyword}`.trim();
+
+  const listSongs: any = [];
+
+  if (keyword) {
+    const keyWordRegex: RegExp = new RegExp(keyword, "i");
+
+    const unidecodeText: string = unidecode(keyword);
+    const keywordSlug = unidecodeText.replace(/\s+/g, "-");
+    const keyWordSlugRegex = new RegExp(keywordSlug, "i");
+
+    const songs = await Song.find({
+      $or: [
+        { slug: keyWordSlugRegex },
+        { title: keyWordRegex }
+      ],
+      deleted: false,
+      status: "active"
+    }).select("avatar title singerId like slug");
+
+    for (const item of songs) {
+      const singer = await Singer.findOne({
+        _id: item.singerId,
+        deleted: false
+      }).select("fullName");
+
+      listSongs.push({
+        id: item.id,
+        avatar: item.avatar,
+        title: item.title,
+        like: item.like,
+        slug: item.slug,
+        singer: {
+          fullName: singer.fullName
+        },
+      });
+    }
+  }
+
+  if(type == "result") {
+    res.render("client/pages/search/result", {
+      pageTitle: `Kết quả: ${keyword}`,
+      keyword: keyword,
+      songs: listSongs
+    });
+  } else {
+    res.json({
+      code: 200,
+      songs: listSongs
+    });
+  }
+};
