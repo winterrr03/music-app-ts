cca2251d5d0cfb4a55acdde8410612d0fc6fda6e
winterrr03 <dong.huynhquang21503@gmail.com>
2024-06-25 14:38:37 +0700

    feat(song): upload audio song

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -6,9 +6,26 @@ if(uploadImage) {
 
   uploadImageInput.addEventListener("change", () => {
     const file = uploadImageInput.files[0];
-    if(file) {
+    if (file) {
       uploadImagePreview.src = URL.createObjectURL(file);
     }
   });
 }
 // End upload-image
+
+// upload-audio
+const uploadAudio = document.querySelector("[upload-audio]");
+if (uploadAudio) {
+  const uploadAudioInput = uploadAudio.querySelector("[upload-audio-input]");
+  const uploadAudioPlay = uploadAudio.querySelector("[upload-audio-play]");
+  const uploadAudioSource = uploadAudioPlay.querySelector("source");
+
+  uploadAudioInput.addEventListener("change", () => {
+    const file = uploadAudioInput.files[0];
+    if (file) {
+      uploadAudioSource.src = URL.createObjectURL(file);
+      uploadAudioPlay.load();
+    }
+  });
+}
+// End upload-audio
